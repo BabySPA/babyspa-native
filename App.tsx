a74b8d248fad18130454cc32025edd574e57267a
Ulann <muzhi.xuan@tuya.com>
2023-07-07 14:20:47 +0800

    feat: 添加nativebase补丁

--- App.tsx
+++ App.tsx
@@ -4,12 +4,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './app/hooks/useCachedResources';
 import useColorScheme from './app/hooks/useColorScheme';
 import Navigation from './app/navigation/RootNavigator';
-import { LogBox } from 'react-native';
-
-// nativeBase warning -> Or to patch-package it
-LogBox.ignoreLogs([
-  'In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app.',
-]);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
