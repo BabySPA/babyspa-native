d57fbcc9cb39ab81bd7df600711a403edbe366dc
Ulanxx <mifind@sina.com>
2023-07-18 09:03:01 +0800

    wip: flow

--- app/screens/flow/components/health-info.tsx
+++ app/screens/flow/components/health-info.tsx
@@ -1,4 +1,17 @@
-import { Box, Column, Row, Image, Text, Divider, TextArea } from 'native-base';
+import { AntDesign } from '@expo/vector-icons';
+import {
+  Box,
+  Column,
+  Row,
+  Image,
+  Text,
+  Divider,
+  TextArea,
+  Center,
+  Icon,
+  Pressable,
+  Menu,
+} from 'native-base';
 import { ImageSourcePropType, TextInput } from 'react-native';
 import { ss, sp } from '~/app/utils/style';
 
@@ -80,7 +93,49 @@ export default function HealthInfo() {
         <BoxItem
           title={'舌部图片'}
           icon={require('~/assets/images/tongue.png')}>
-          <Box>ss</Box>
+          <Menu
+            w={ss(200)}
+            _text={{ fontSize: sp(18), color: '#000' }}
+            trigger={(triggerProps) => {
+              return (
+                <Pressable {...triggerProps}>
+                  <Center
+                    borderColor={'#ACACAC'}
+                    borderWidth={1}
+                    borderStyle={'dashed'}
+                    bgColor={'#FFF'}
+                    w={ss(100)}
+                    h={ss(100)}>
+                    <Icon
+                      as={<AntDesign name='plus' size={ss(40)} />}
+                      color={'#ACACAC'}
+                    />
+                  </Center>
+                </Pressable>
+              );
+            }}>
+            <Menu.Item alignItems={'center'} py={ss(16)}>
+              <Text fontWeight={600} justifyContent={'center'}>
+                请选择上传方式
+              </Text>
+            </Menu.Item>
+            <Menu.Item
+              borderTopColor={'#DFE1DE'}
+              borderTopWidth={1}
+              justifyContent={'center'}
+              alignItems={'center'}
+              py={ss(16)}>
+              <Text textAlign={'center'}>立即拍摄</Text>
+            </Menu.Item>
+            <Menu.Item
+              borderTopColor={'#DFE1DE'}
+              borderTopWidth={1}
+              justifyContent={'center'}
+              alignItems={'center'}
+              py={ss(16)}>
+              <Text textAlign={'center'}>从相册选择</Text>
+            </Menu.Item>
+          </Menu>
         </BoxItem>
         <BoxItem
           title={'手部图片'}
