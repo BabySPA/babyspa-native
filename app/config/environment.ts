f9ae08f704e09d0bf7b2837870521bc3c35edcab
Ulanxx <mifind@sina.com>
2023-09-24 23:37:31 +0800

    feat: web

--- app/config/environment.ts
+++ app/config/environment.ts
@@ -3,7 +3,7 @@ import { MANAGER_API_URL } from '@env';
 
 const Environment = {
   api: {
-    manager: 'http://121.36.108.137:4000/manager',
+    manager: '121.36.108.137:4000/manager',
     // manager: 'http://127.0.0.1:4000/manager',
   },
 };
