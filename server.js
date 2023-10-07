f2283817a2cc44c762e67a062ad2b856df7bfa0f
Ulanxx <mifind@sina.com>
2023-10-07 08:17:01 +0800

    chore: pm2 启动

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 4300;
 // 加载dist目录下的index.html文件
-app.use(express.static('dist'));
+app.use('/management', express.static('dist'));
 // 启动服务器
 app.listen(port, () => console.log(`Listening on port ${port}`));
