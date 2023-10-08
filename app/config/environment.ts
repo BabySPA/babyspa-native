21eaeb7a7d5199b58e14fdc2d3a43adf85569c74
Ulanxx <mifind@sina.com>
2023-10-08 23:58:07 +0800

    chore: 优化

--- app/config/environment.ts
+++ app/config/environment.ts
@@ -3,8 +3,8 @@ import { MANAGER_API_URL } from '@env';
 
 const Environment = {
   api: {
-    manager: 'http://47.110.41.229:4000/manager',
-    // manager: 'https://mcbabyspa.com/api/manager',
+    // manager: 'http://121.36.108.137:4000/manager',
+    manager: 'http://127.0.0.1:4000/manager',
   },
 };
 
